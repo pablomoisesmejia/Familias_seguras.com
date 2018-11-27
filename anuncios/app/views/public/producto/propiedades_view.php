0975cea1b93cd6263a9a272c543f7f4614de17ac
eduardozu666 <38825902+eduardozu666@users.noreply.github.com>
2018-11-27 16:17:24 -0600

    Cards
    nuevas cards

--- anuncios/app/views/public/producto/propiedades_view.php
+++ anuncios/app/views/public/producto/propiedades_view.php
@@ -1,31 +1,32 @@
 <div class='container'>
-<h5 style='font-size:1.4em;' class='left-align'>Vehiculos Asegurables en Venta</h5>
+<h5 style='font-size:1.4em;' class='left-align'>Propiedades en Venta</h5>
     <div class='row'>
 
+                     
     
             <!-- codigo de card -->
             <div class='col s12 m12 l4'>
                 <div class='card hoverable'>
                     <div class='card-image waves-effect waves-block waves-light'>
-                    <img class='activator' src='../web/img/propiedades/prop1.jpg'>
+                    <img class='activator' src='../web/img/propiedades/prop1.jpg'> 
                     </div>
+                   
                     <div class='purple darken-3'>
-                        
-                        <a style='color:white;' href='pagina.php'>
-                            <div class='block_name'>
-                                <div class='row' style='padding:0; margin:0;  font-size:0.9em'>CV-201811_0001</div>
-                                <div class='row' style='padding:0; margin:0; font-size:0.7em'>BMW Sedan x3-2017</div>
+                        <a style='color:white; height:80px;' href='pagina.php'>
+                            <div class='col s7' id='previnfo_vehi'>
+                                <div class='row' style='padding:0; margin:0;  font-size:1em'>CV-201811_0002</div>
+                                <div class='row' id='name_dir_style' style='padding:0; margin:0; font-size:0.8em'>Colonia Lourdes AV. Pacifico</div>
                             </div>
-                            <div class='block_proper'>
-                                <div class='col s4 icon_prop'><img class='proper_ico' src='../web/img/ico/garage.png'>1</div>
-                                <div class='col s4 icon_prop'><img class='proper_ico' src='../web/img/ico/banera.png'>2</div>
-                                <div class='col s4 icon_prop'><img class='proper_ico' src='../web/img/ico/bed.png'>1</div>
-                            </div>
-                            <div class='block_price'>
-                                <div style='text-align:right; font-size:1em; padding-top:12px; padding-bottom:12px;'>$13,000.00</div>
+                            <div class=''>
+                                <div class='row' style='text-align:center; font-size:1.2em; padding-top:12px;'>$13,000.00</div>
                             </div>
                         </a>
                     </div>
+                    <div class='block_proper'>
+                                <div class=' icon_prop'><img class='proper_ico' src='../web/img/ico/garage.png'><span class='proper_ico_txt'>1</span></div>
+                                <div class='icon_prop'><img class='proper_ico' src='../web/img/ico/banera.png'><span class='proper_ico_txt'>2</span></div>
+                                <div class='icon_prop'><img class='proper_ico' src='../web/img/ico/bed.png'><span class='proper_ico_txt'>4</span></div>
+                    </div> 
                 </div>
             </div>
             <!-- fin code -->
