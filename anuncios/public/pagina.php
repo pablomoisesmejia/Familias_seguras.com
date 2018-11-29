1cf2e5e565f10a70da5cdab44308a6069e694b6f
pablomoisesmejia <38825955+pablomoisesmejia@users.noreply.github.com>
2018-11-29 15:52:28 -0600

    emergencia

---
+++ anuncios/public/pagina.php
@@ -0,0 +1,6 @@
+<?php
+require_once("../app/views/public/templates/page.class.php");
+Page::templateHeader("Directorio");
+require_once("../app/views/public/producto/detalle_view.1.php");
+Page::templateFooter();
+?>
--- anuncios/public/productos.php
+++ anuncios/public/productos.php
@@ -1,8 +1,6 @@
 <?php
 require_once("../app/views/public/templates/page.class.php");
 Page::templateHeader("Directorio");
-
 require_once("../app/controllers/public/producto/productos_controller.php");
-
 Page::templateFooter();
 ?>
