e223c90aa6714153f60cbed1a371538b47f44c74
pablomoisesmejia <38825955+pablomoisesmejia@users.noreply.github.com>
2018-12-09 18:28:04 -0600

    referebcia

--- anuncios/app/views/public/producto/propiedades_view.php
+++ anuncios/app/views/public/producto/propiedades_view.php
@@ -1,7 +1,7 @@
 <div class='container'>
 <div class='row'>
     <div class='col s12 m6'><h5 style='font-size:1.36em;' class='left-align'>Propiedades en Venta</h5></div>
-    <div class='col s12 m6'><a id='btn_anunciate' class='right-align'>Añade tu Anuncio</a></div>
+    <div class='col s12 m6'><a id='btn_anunciate'  href="../dashboard/account/register.php"  class='right-align'>Añade tu Anuncio</a></div>
     </div>
     <div class='row'>
 
