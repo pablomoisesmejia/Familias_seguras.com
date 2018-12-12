a1c340a9c2ad9898e6c7cbd7e1a2b4679eed322e
XavierCanjura <36569554+XavierCanjura@users.noreply.github.com>
2018-12-11 23:10:45 -0600

    calendario diseño mobile

--- anuncios/app/views/public/producto/calendario_view.php
+++ anuncios/app/views/public/producto/calendario_view.php
@@ -3,35 +3,45 @@
 <!--modal para agregar, modificar y eliminar citas-->
 <div id="modalCitas" class="modal modal-fixed-footer">
     <div class="modal-content">
-        <h4 class="modal-title" id="titulo">Agregar Evento</h4>
+        <h4 class="modal-title" id="titulo">Programar Cita</h4>
         <div class="divider"></div>
-        <div class="row">
-            <div class="col s6" id="id_cita">
-                <h6>Id:</h6>
-                <input type="text" id="id" name="id" class="validate"/>
+        <form autocomplete="off">
+            <div class="row">
+                <div class='input-field col s12 l6'>
+                    <i style="color:black;"class="material-icons prefix">event_note</i>
+                    <input id="fecha" type="text" class="validate" disabled required/>
+                    <label class="active" id='fecha-label' for="fecha">Fecha</label>
+                </div>
+                <div class='input-field col s12 l6'>
+                    <i style="color:black;"class="material-icons prefix">event_note</i>
+                    <input id="hora" type="text" class="timepicker validate" required/>
+                    <label class="" for="hora">Hora</label>
+                </div>
+                <div class='input-field col s12 l6'>
+                    <i class='material-icons prefix'>person</i>
+                    <input id='nombres' type='text' name='nombres' class='validate' required/>
+                    <label for='nombres'>Nombres</label>
+                </div>
+                <div class='input-field col s12 l6'>
+                    <i class='material-icons prefix'>email</i>
+                    <input id='correo' type='email' name='correo' class='validate' required/>
+                    <label for='correo'>Correo eletrónico</label>
+                </div>
+                <div class='input-field col s12 l6'>
+                    <i class='material-icons prefix'>person</i>
+                    <input id='lugar_reunion' type='text' name='lugar_reunion' class='validate' required/>
+                    <label for='lugar_reunion'>Lugar de Reunión</label>
+                </div>
+                <div class="input-field col s12 l12">
+                    <i style="color:black;"class="material-icons prefix">chat</i>
+                    <textarea id="asunto" name='asunto' class="materialize-textarea"></textarea>
+                    <label for="asunto">Asunto</label>
+                </div>
             </div>
-            <div class="col s12">
-                <h6>Titulo:</h6>
-                <input type="text" id="tituloEvento" name="tituloEvento" class="validate"/>
-            </div>
-            <div class="col s6">
-                <h6>Fecha:</h6>
-                <input type="text" id="fecha" name="fecha" class="validate" disabled/>
-            </div>
-            <div class="col s6">
-                <h6>Hora:</h6>
-                <input type="text" id="hora" name="hora" class="timepicker">
-            </div>
-            <div class="col s12">
-                <h6>Descripcion:</h6>
-                <textarea id="descripcionEvento" name="descripcionEvento" class="materialize-textarea" class="validate"></textarea>
-            </div>
-        </div>
+        </form>
     </div>
     <div class="modal-footer">
         <a id="agregar" class="waves-effect waves-light btn btn-small">Agregar</a>
-        <a id="modificar" class="waves-effect waves-light btn btn-small">Modificar</a>
-        <a id="eliminar" class="waves-effect waves-light btn btn-small red">Eliminar</a>
         <a class="modal-action modal-close waves-effect waves-grey btn btn-small grey">Cancelar</a>
     </div>
 </div>
--- anuncios/web/js/calendario.js
+++ anuncios/web/js/calendario.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
 
+    setTimeout(function(){ $('#fecha-label').addClass('active'); }, 1);
+
+    $('.timepicker').pickatime({
+        default: 'now', // Set default time: 'now', '1:30AM', '16:30'
+        fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
+        twelvehour: false, // Use AM/PM or 24-hour format
+        donetext: 'OK', // text for done-button
+        cleartext: 'Limpiar', // text for clear-button
+        canceltext: 'Cancelar', // Text for cancel-button,
+        container: undefined, // ex. 'body' will append picker to body
+        autoclose: false, // automatic close timepicker
+        ampmclickable: true, // make AM PM clickable
+        aftershow: function(){} //Function for after opening timepicker
+    });
+    
     $('#calendario').fullCalendar({
         header:{
             left:'prev,today,next',
